b260a274d794e05aa9ab2daac98fa928075013db
Jessica Lu <jlu.astro@yahoo.com>
2022-06-09 10:54:03 -0700

    Tests for new data.py

---
+++ tests/test_data.py
@@ -0,0 +1,26 @@
+from BAGLE import data
+import pytest
+
+def test_EventDataDict_init():
+
+    # This should throw an exception
+    with pytest.raises(Exception):
+        data_dict = data.EventDataDict()
+
+    # Create a valid one.
+    data_in = {'target_name': 'OB110462',
+               'raL': '17:51:40.19',
+               'decL': '-29:53:26.3'}
+        
+    data_dict = data.EventDataDict(data_in)
+
+    assert data_dict['target_name'] == data_in['target_name']
+    assert data_dict['raL'] == data_in['raL']
+    assert data_dict['decL'] == data_in['decL']
+    assert data_dict['phot_files'] == []
+    assert data_dict['phot_data'] == []
+    assert data_dict['ast_files'] == []
+    assert data_dict['ast_data'] == []
+    
+    return
+
