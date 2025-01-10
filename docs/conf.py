c3e2914796c5d2c6251b2e5e33bff0c875a34566
Macy Huston <mhuston@laser.astro.berkeley.edu>
2025-01-10 15:49:33 -0800

    trying to deal with graphviz problem

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ html_static_path = ['_static']
 # a list of builtin themes.
 #
 import sphinx_rtd_theme
-from graphviz import dot
+#from graphviz import dot
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
